a6ff6c0aca752fc942d964c7dca864460fc91707
Beau Hunter <hunter0907@gmail.com>
2019-02-05 18:28:25 -0700

    Update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,11 +11,10 @@ var connection = mysql.createConnection({
     user: "root",
 
     // Your password
-    password: "",
+    password: "Rock$tar21",
     database: "bamazon_db"
 
 });
-
 
 connection.connect(function (err) {
     // console.log("Connected as id: "+ connection.threadId);
@@ -23,126 +22,147 @@ connection.connect(function (err) {
 
 });
 
-var start = function() {
-
-var data = [{
-        item_id: 1,
-        product_name: "Xbox",
-        department_name: "Electronics",
-        price: 250.50,
-        stock_quantity: 150
-    },
-    {
-        item_id: 2,
-        product_name: "Fishing Pole",
-        department_name: "Sporting Goods",
-        price: 29.99,
-        stock_quantity: 80
-    },
-    {
-        item_id: 3,
-        product_name: "Beef Jerky",
-        department_name: "Grocery",
-        price: 1.99,
-        stock_quantity: 10000
-    },
-    {
-        item_id: 4,
-        product_name: "Paint Brush",
-        department_name: "Paint",
-        price: 5.00,
-        stock_quantity: 50
-    },
-    {
-        item_id: 5,
-        product_name: "Extension Cord",
-        department_name: "Electrical",
-        price: 10.00,
-        stock_quantity: 15
-    },
-    {
-        item_id: 6,
-        product_name: "Drill",
-        department_name: "Hardware",
-        price: 199.99,
-        stock_quantity: 10
-    },
-    {
-        item_id: 7,
-        product_name: "Sheet Rock",
-        department_name: "Building Materials",
-        price: 12.55,
-        stock_quantity: 10
-    },
-    {
-        item_id: 8,
-        product_name: "Piping",
-        department_name: "Plumbing",
-        price: 9.97,
-        stock_quantity: 10
-    },
-    {
-        item_id: 9,
-        product_name: "Hose",
-        department_name: "Garden",
-        price: 24.97,
-        stock_quantity: 15
-    },
-    {
-        item_id: 10,
-        product_name: "Bike",
-        department_name: "Outdoors",
-        price: 375.75,
-        stock_quantity: 7
-    }
-]
-
-var table = new Table
-
-data.forEach(function (products) {
-    table.cell('Item ID', products.item_id)
-    table.cell('Product', products.product_name)
-    table.cell('Department', products.department_name)
-    table.cell('Price', products.price, Table.number(2))
-    table.cell('Stock', products.stock_quantity)
-    table.newRow()
-})
-
-console.log(table.toString())
-
-inquirer
-    .prompt([
-        {
-			type: 'input',
-			name: 'item_id',
-            message: 'Enter ID of product:',
-            validate: function(value) {
-                if (isNaN(value) == false) {
-                    return true
-                } else {
-                    return false;
-                }
+var start = function () {
+    
+        var data = [{
+                item_id: 1,
+                product_name: "Xbox",
+                department_name: "Electronics",
+                price: 250.50,
+                stock_quantity: 150
+            },
+            {
+                item_id: 2,
+                product_name: "Fishing Pole",
+                department_name: "Sporting Goods",
+                price: 29.99,
+                stock_quantity: 80
+            },
+            {
+                item_id: 3,
+                product_name: "Beef Jerky",
+                department_name: "Grocery",
+                price: 1.99,
+                stock_quantity: 100
+            },
+            {
+                item_id: 4,
+                product_name: "Paint Brush",
+                department_name: "Paint",
+                price: 5.00,
+                stock_quantity: 50
+            },
+            {
+                item_id: 5,
+                product_name: "Extension Cord",
+                department_name: "Electrical",
+                price: 10.00,
+                stock_quantity: 15
+            },
+            {
+                item_id: 6,
+                product_name: "Drill",
+                department_name: "Hardware",
+                price: 199.99,
+                stock_quantity: 10
+            },
+            {
+                item_id: 7,
+                product_name: "Sheet Rock",
+                department_name: "Building Materials",
+                price: 12.55,
+                stock_quantity: 10
+            },
+            {
+                item_id: 8,
+                product_name: "Piping",
+                department_name: "Plumbing",
+                price: 9.97,
+                stock_quantity: 10
+            },
+            {
+                item_id: 9,
+                product_name: "Hose",
+                department_name: "Garden",
+                price: 24.97,
+                stock_quantity: 15
+            },
+            {
+                item_id: 10,
+                product_name: "Bike",
+                department_name: "Outdoors",
+                price: 375.75,
+                stock_quantity: 12
             }
+        ]
 
-		},
-		{
-			type: 'input',
-			name: 'quantity',
-            message: 'How many do you need?',
-            validate: function(value) {
-                if (isNaN(value) == false) {
-                    return true
-                } else {
-                    return false;
+        var table = new Table
+
+        data.forEach(function (products) {
+            table.cell('Item ID', products.item_id)
+            table.cell('Product', products.product_name)
+            table.cell('Department', products.department_name)
+            table.cell('Price', products.price, Table.number(2))
+            table.cell('Stock', products.stock_quantity)
+            table.newRow()
+        })
+
+        console.log(table.toString())
+        console.log("WELCOME TO BAMAZON!")
+        inquirer
+            .prompt([{
+                    type: 'input',
+                    name: 'item_id',
+                    message: 'Enter ID of product:',
+                    validate: function (value) {
+                        if (isNaN(value) == false) {
+                            return true
+                        } else {
+                            return false;
+                        }
+                    }
+
+                },
+                {
+                    type: 'input',
+                    name: 'quantity',
+                    message: 'How many do you need?',
+                    validate: function (value) {
+                        if (isNaN(value) == false) {
+                            return true
+                        } else {
+                            return false;
+                        }
+                    }
                 }
+            ]).then(function (answer) {
+
+                    var idPicked = answer.item_id;
+                    var quantityPicked = answer.quantity;
+                    
+
+                    if(quantityPicked < idPicked.stock_quantity){
+                            console.log("Your item has been ordered"  )
+                            connection.query( " Update SELECT from products")
+                    }else{
+                        console.log("Not enough on hands")
             }
-		}
-    ]).then(function(data){
-        connection.query(function (err) {
-            if (err) throw err;
-        });
-
-    });
-}
-
-start();
+              
+                })
+            }
+                // wantMore();
+                // function wantMore(){
+                //     inquirer
+                //     .prompt([{
+                //             type: 'confirm',
+                //             name: 'reply',
+                //             message: 'Would you like to buy more?',
+                //     }]).then(function(answer){
+                //         if(answer.reply){
+                //             start();
+                //         } else {
+                //             console.log("Please come again!")
+                //         }
+                //     })
+                // }
+            start()
